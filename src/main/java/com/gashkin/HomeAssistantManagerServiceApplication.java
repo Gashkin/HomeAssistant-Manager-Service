04c8a8492a8115b38613c83252345ffe43c3ba5e
Gashkin <gergashkin@gmail.com>
2025-11-02 07:18:28 +0000

    Add main Spring Boot application class

---
+++ src/main/java/com/gashkin/HomeAssistantManagerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.gashkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAssistantManagerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeAssistantManagerServiceApplication.class, args);
+    }
+}
